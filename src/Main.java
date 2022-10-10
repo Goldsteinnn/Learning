384b254c5f2e4bc1e46158730ad435e75058f2b4
goldsteinnn <huynhgoldstein@gmail.com>
2022-10-10 12:09:24 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+        methods meth = new methods();
+        int sum = meth.addition(1,2);
+        System.out.println(sum);
+    }
+}
---
+++ src/methods.java
@@ -0,0 +1,5 @@
+public class methods {
+    public int addition(int x, int y){
+        return x + y;
+    }
+}
