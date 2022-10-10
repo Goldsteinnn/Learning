5166e2c11012cd80c6ed2e4d2cce0d4ca05959e6
goldsteinnn <huynhgoldstein@gmail.com>
2022-10-10 12:30:08 -0500

    Added divide in sub branch

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println(sum);
         int sub = meth.subtraction(2,1);
         System.out.println(sub);
+        int div = meth.divide(2,0);
+        System.out.println(div);
     }
 }
--- src/methods.java
+++ src/methods.java
@@ -6,4 +6,15 @@ public class methods {
     public int subtraction(int x, int y){
         return x - y;
     }
+
+    public int divide(int  x , int y){
+        try{
+            return x/y;
+        }
+        catch (ArithmeticException e){
+            System.out.println("Cannot divide by zero");
+            return 0;
+        }
+    }
 }
+
