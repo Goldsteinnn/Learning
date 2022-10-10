4d9c0e4d0d74ee0706e35222f1412512844bd914
goldsteinnn <huynhgoldstein@gmail.com>
2022-10-10 13:07:23 -0500

    added mod function

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,7 @@ public class Main {
         System.out.println(sub);
         int mult = meth.multiplication(2,0);
         System.out.println(mult);
+        int mod = meth.modu(4,1);
+        System.out.println(mod);
     }
 }
--- src/methods.java
+++ src/methods.java
@@ -6,8 +6,11 @@ public class methods {
     public int subtraction(int x, int y){
         return x - y;
     }
-
     public int multiplication(int x , int y){
         return x*y;
+    }
+
+    public int modu (int x, int y){
+        return x%y;
     }
 }
